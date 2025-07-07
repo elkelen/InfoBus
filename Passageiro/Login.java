96969e3576d38135400635f96c0ebd62929e7ca1
Anna <barbosannas@gmail.com>
2025-07-07 17:55:31 -0300

    ok

---
+++ Passageiro/Login.java
@@ -0,0 +1,32 @@
+package infobus;
+
+import java.util.Date;
+import java.util.ArrayList;
+
+public class Login {
+    private String email;
+    private String senha;
+
+    public Login(String email, String senha) {
+        this.email = email;
+        this.senha = senha;
+    }
+
+    public boolean verificarSenha(String senha) {
+        return this.senha.equals(senha);
+    }
+
+    public String getEmail() { 
+        return email; 
+    }
+    public void setEmail(String email) { 
+        this.email = email; 
+    }
+    public String getSenha() { 
+        return senha; 
+    }
+    public void setSenha(String senha) { 
+        this.senha = senha; 
+    }
+}
+
